e5fc71275ea5cde588031da92e597c3bbc20314f
Dana Luzina <danaluzina@yandex.ru>
2025-07-19 01:59:19 +0300

    UI tests and allure report

---
+++ src/main/java/pageObject/RestorePasswordPage.java
@@ -0,0 +1,29 @@
+package pageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class RestorePasswordPage {
+    WebDriver webDriver;
+
+    public RestorePasswordPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    private final By restoreFormHeading = By.xpath(".//*[text()='Восстановление пароля']");
+
+    private final By rememberedPasswordLink = By.xpath(".//a[text()='Войти']");
+
+    public void waitForPageLoad(){
+        new WebDriverWait(webDriver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(restoreFormHeading));
+    }
+
+    public void clickRememberedPassword(){
+        webDriver.findElement(rememberedPasswordLink).click();
+    }
+}
+
